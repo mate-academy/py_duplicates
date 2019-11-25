a2ca6c655d6e7bb9808f9d3fbf546eb85bcfd933
Senku1 <morhaliuk@gmail.com>
2019-11-25 10:37:43 +0200

    Solved

--- duplicates.py
+++ duplicates.py
@@ -1,6 +1,11 @@
+"""Docstring"""
 from typing import List
 
 
-def find_duplicates(l: List[int]) -> int:
-
-    return 42
+def find_duplicates(lis: List[int]) -> int:
+    """
+    Returns number of duplicates in the given list
+    :param lis: List[int]
+    :return: int
+    """
+    return len({x for x in lis if lis.count(x) > 1})
