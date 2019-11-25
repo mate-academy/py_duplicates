e0f98d01052015c1ee4d07ddf1abb45107df155d
DukhDmytro <cowboybebop4991@gmail.com>
2019-11-25 10:20:39 +0200

    1st commit

--- duplicates.py
+++ duplicates.py
@@ -2,5 +2,16 @@ from typing import List
 
 
 def find_duplicates(l: List[int]) -> int:
+    duplicates = []
+    unique = []
+    duplicate_count = 0
+    for element in l:
+        if element not in unique:
+            unique.append(element)
+        else:
+            if element not in duplicates:
+                duplicates.append(element)
+                duplicate_count += 1
 
-    return 42
+    return duplicate_count
+
