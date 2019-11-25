d12f328245abc8e3cd2314a4d065bc80efeb7547
d1makov <makovskyj.danylo@gmail.com>
2019-11-25 10:25:57 +0200

    final

--- duplicates.py
+++ duplicates.py
@@ -1,6 +1,14 @@
-from typing import List
+"""
+docstring
+"""
 
 
-def find_duplicates(l: List[int]) -> int:
+def find_duplicates(lst) -> int:
+    """
 
-    return 42
+    :param lst:
+    :return:
+    """
+    # new list with items that appends more than one time
+    new_lst = [item for index, item in enumerate(lst) if lst.count(item) > 1]
+    return len(set(new_lst))
