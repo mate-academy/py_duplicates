ef92a6cd92b282f04fa799744b00ef3550d81a74
Dima <dima.sapsay@gmail.com>
2019-11-25 10:12:08 +0200

    first commit

--- duplicates.py
+++ duplicates.py
@@ -1,6 +1,14 @@
+"""
+Find count duplicate elements in  a list
+"""
+
 from typing import List
 
 
-def find_duplicates(l: List[int]) -> int:
-
-    return 42
+def find_duplicates(lst: List[int]) -> int:
+    """Find count"""
+    count = 0
+    for value in set(lst):
+        if lst.count(value) > 1:
+            count += 1
+    return count
