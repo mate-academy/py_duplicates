d4b3dfc638daedcedaf5261d88ff7d3803fbf118
Andrii <andrewsotnikov88@gmail.com>
2019-11-25 23:11:59 +0200

    Duplicate elements in a list

--- duplicates.py
+++ duplicates.py
@@ -1,6 +1,7 @@
+"""Count duplicate elements in a list"""
 from typing import List
 
 
-def find_duplicates(l: List[int]) -> int:
-
-    return 42
+def find_duplicates(values: List[int]) -> int:
+    """Method that count duplicate elements in a list"""
+    return len(set({_ for _ in values if values.count(_) > 1}))
