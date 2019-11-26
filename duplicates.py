fb0445432de20030ccd66ed209a57c79c189cb86
Platoniuks <platon@alterpost.org>
2019-11-26 08:45:20 +0200

    resolved

--- duplicates.py
+++ duplicates.py
@@ -1,6 +1,13 @@
+"""Count duplicate elements in  a list."""
+
 from typing import List
 
 
-def find_duplicates(l: List[int]) -> int:
-
-    return 42
+def find_duplicates(arr: List[int]) -> int:
+    """General func"""
+    amout_duplicate = 0
+    items_set = set(arr)
+    for i in items_set:
+        if arr.count(i) > 1:
+            amout_duplicate += 1
+    return amout_duplicate
