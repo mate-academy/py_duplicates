befb3f1409a671532f717c7ccba13c8bf7bd3744
DukhDmytro <cowboybebop4991@gmail.com>
2019-12-01 00:19:53 +0200

    2nd commit

--- duplicates.py
+++ duplicates.py
@@ -3,16 +3,5 @@ from typing import List
 
 
 def find_duplicates(lst: List[int]) -> int:
-    """doc"""
-    duplicates = []   # type: List[int]
-    unique = []   # type: List[int]
-    duplicate_count = 0
-    for element in lst:
-        if element not in unique:
-            unique.append(element)
-        else:
-            if element not in duplicates:
-                duplicates.append(element)
-                duplicate_count += 1
-
-    return duplicate_count
+    """return number of duplicates in list"""
+    return len({item for item in set(lst) if lst.count(item) > 1})
